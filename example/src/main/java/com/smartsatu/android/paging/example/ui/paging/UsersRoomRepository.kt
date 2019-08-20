18e41215cd55b985d23f4f53bf804ca3a99cf475
Daniil Lukashevich <danaimset@gmail.com>
2019-08-20 16:10:11 +0300

    [example] 5. Declare data room repository and implement required methods; Override clearRoom method which allows to clear data by specified params

---
+++ example/src/main/java/com/smartsatu/android/paging/example/ui/paging/UsersRoomRepository.kt
@@ -0,0 +1,31 @@
+package com.smartsatu.android.paging.example.ui.paging
+
+import androidx.paging.DataSource
+import com.smartsatu.android.paging.PagingRoomRepository
+import com.smartsatu.android.paging.example.data.UsersRepository
+import com.smartsatu.android.paging.example.data.local.model.User
+import io.reactivex.Observable
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+class UsersRoomRepository(private val usersRepository: UsersRepository) : PagingRoomRepository<UserParams, User>() {
+    override fun clearRoom() {
+        // Legacy
+    }
+
+    override fun clearRoom(params: UserParams) {
+        usersRepository.room.userDao().deleteAll()
+    }
+
+    override fun provideNetworkExecutor(): Executor {
+        return Executors.newSingleThreadExecutor()
+    }
+
+    override fun provideDataSourceFactory(params: UserParams): DataSource.Factory<Int, User> {
+        return usersRepository.room.userDao().queryAll()
+    }
+
+    override fun requestAndStoreData(params: UserParams): Observable<List<User>> {
+        return usersRepository.requestUsers(params.page, params.pageSize)
+    }
+}
